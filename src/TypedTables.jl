e92137f9db638097899d8a146202c95deaecd1c5
Andy Ferris <andy.ferris@roames.com.au>
2018-11-19 20:42:32 +1000

    Horribe workaround for inference of NamedTuple constructor
    Mostly fixes #34. I think I can live with waiting for a proper fix of
    JuliaLang/julia#29970 for more than 32 columns.

--- src/TypedTables.jl
+++ src/TypedTables.jl
@@ -32,6 +32,18 @@ end
 _ndims(a::NamedTuple{<:Any, T}) where {T} = _ndims(T)
 _ndims(::Type{<:Tuple{Vararg{AbstractArray{<:Any, n}}}}) where {n} = n
 
+# Workaround for JuliaLang/julia#29970
+let
+    for n in 1:32
+        Ts = [Symbol("T$i") for i in 1:n]
+        xs = [:(x[$i]) for i in 1:n]
+        NT = :(Core.NamedTuple{names, Tuple{$(Ts...)}})
+        eval(quote
+            $NT(x::Tuple{$(Ts...)}) where {names, $(Ts...)} = $(Expr(:new, NT, xs...))
+        end)
+    end
+end
+
 include("Table.jl")
 include("FlexTable.jl")
 include("columnops.jl")
