2d0152b3cf82f4ac430ff9f84f3ec29e900d95e2
Andy Ferris <andy.ferris@roames.com.au>
2018-11-26 16:56:25 +1000

    Add filter, findall, etc

--- src/columnops.jl
+++ src/columnops.jl
@@ -18,10 +18,12 @@ end
 end
 
 @inline function Base.map(f::Calc{names}, t::Table) where {names}
+    # minimize number of columns before iterating over the rows
     map(f, GetProperties(names)(t))
 end
 
 @inline function Base.map(f::Calc{names}, t::Table{<:NamedTuple{names}}) where {names}
+    # efficient to iterate over rows with a minimal number of columns
     invoke(map, Tuple{Function, typeof(t)}, f, t)
 end
 
@@ -47,10 +49,12 @@ end
 end
 
 @inline function SplitApplyCombine.mapview(f::Calc{names}, t::Table) where {names}
+    # minimize number of columns before iterating over the rows
     mapview(f, GetProperties(names)(t))
 end
 
 @inline function SplitApplyCombine.mapview(f::Calc{names}, t::Table{<:NamedTuple{names}}) where {names}
+    # efficient to iterate over rows with a minimal number of columns
     invoke(mapview, Tuple{Function, typeof(t)}, f, t)
 end
 
@@ -74,8 +78,14 @@ end
     return f(t)
 end
 
-@inline function Broadcast.broadcasted(::Broadcast.DefaultArrayStyle{N}, f::Calc, t::Table{<:Any, N}) where {N}
-    return Broadcast.Broadcasted{Broadcast.DefaultArrayStyle{N}}(f, (t,), axes(t))
+@inline function Broadcast.broadcasted(style::Broadcast.DefaultArrayStyle{N}, f::Calc{names}, t::Table{<:NamedTuple, N}) where {N, names}
+    # minimize number of columns before iterating over the rows
+    return Broadcast.broadcasted(style, f, GetProperties(names)(t))
+end
+
+@inline function Broadcast.broadcasted(style::Broadcast.DefaultArrayStyle{N}, f::Calc{names}, t::Table{<:NamedTuple{names}, N}) where {N, names}
+    # efficient to iterate over rows with a minimal number of columns
+    invoke(Broadcast.broadcasted, Tuple{typeof(style), Function, typeof(t)}, style, f, t)
 end
 
 @inline function Broadcast.broadcasted(::Broadcast.DefaultArrayStyle{N}, f::Select, t::Table{<:Any, N}) where {N}
@@ -87,6 +97,37 @@ Broadcast.materialize(t::Table) = Table(map(_materialize, columns(t)))
 _materialize(x) = Broadcast.materialize(x)
 _materialize(x::MappedArray) = copy(x)
 
-# TODO filter
+# mapreduce
 
-# TODO mapreduce
+function Base.mapreduce(f::Calc{names}, op, t::Table; kwargs...) where {names}
+    # minimize number of columns before iterating over the rows
+    t2 = GetProperties(names)(t)
+    return mapreduce(f, op, t; kwargs...)
+end
+
+function Base.mapreduce(f::Calc{names}, op, t::Table{names}; kwargs...) where {names}
+    # efficient to iterate over rows with a minimal number of columns
+    invoke(mapreduce, Tuple{Function, typeof(of), typeof(t)}, f, op, t; kwargs...)
+end
+
+# `filter(f, t)` defaults to `t[map(f, t)]`
+
+function Base.filter(f::GetProperty, t::Table)
+    return @inbounds t[f(t)::AbstractArray{Bool}]
+end
+
+# findall
+
+function Base.findall(f::GetProperty, t::Table)
+    return findall(identity, f(t))
+end
+
+function Base.findall(f::Calc{names}, t::Table) where {names}
+    # minimize number of columns before iterating over the rows
+    return findall(f, GetProperties(names)(t))
+end
+
+function Base.findall(f::Calc{names}, t::Table{<:NamedTuple{names}}) where {names}
+    # efficient to iterate over rows with a minimal number of columns
+    invoke(findall, Tuple{Function, typeof(t)}, f, t)
+end
